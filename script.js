4ac5f5768f1be1bb3b556e083c5d8aab70e095b2
demo <37427036+dannydw1@users.noreply.github.com>
2022-04-28 02:15:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -622,10 +622,48 @@ loader.load("/assets/tree.glb", (glb) => {
 }, (xhr) => { }, (error) => { }
 )
 
-
-loader.load("/assets/plant.glb", (glb) => {
+loader.load("/assets/tree.glb", (glb) => {
   var model = glb.scene
-  model.position.set(-16.3, 0, -0.2)
+  model.position.set(-5.3, 0, 4)
+  model.scale.set(3, 3, 3)
+  model.traverse(function(e) {
+    if (e.isMesh) {
+      e.castShadow = true;
+      e.receiveShadow = true
+    }
+  })
+  scene.add(model)
+}, (xhr) => { }, (error) => { }
+)
+loader.load("/assets/tree.glb", (glb) => {
+  var model = glb.scene
+  model.position.set(-5.3, 0, 1)
+  model.scale.set(3, 3, 3)
+  model.traverse(function(e) {
+    if (e.isMesh) {
+      e.castShadow = true;
+      e.receiveShadow = true
+    }
+  })
+  scene.add(model)
+}, (xhr) => { }, (error) => { }
+)
+loader.load("/assets/tree.glb", (glb) => {
+  var model = glb.scene
+  model.position.set(-5.3, 0, -1)
+  model.scale.set(3, 3, 3)
+  model.traverse(function(e) {
+    if (e.isMesh) {
+      e.castShadow = true;
+      e.receiveShadow = true
+    }
+  })
+  scene.add(model)
+}, (xhr) => { }, (error) => { }
+)
+loader.load("/assets/tree.glb", (glb) => {
+  var model = glb.scene
+  model.position.set(-16.3, 0, -3)
   model.scale.set(1.5, 1.5, 1.5)
   model.traverse(function(e) {
     if (e.isMesh) {
