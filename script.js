4ce02dfe033028d63fa33a4dbbd4a78fb2b468df
demo <37427036+dannydw1@users.noreply.github.com>
2022-04-28 01:52:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -610,7 +610,7 @@ loader.load("/assets/cart.glb", (glb) => {
 
 loader.load("/assets/tree.glb", (glb) => {
   var model = glb.scene
-  model.position.set(1, 1, 1)
+  model.position.set(-5.3, 0, -0.2)
   model.scale.set(3, 3, 3)
   model.traverse(function(e) {
     if (e.isMesh) {
