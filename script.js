898fbc1e48d656530c5e66c2338baaa6bedaa66d
demo <37427036+dannydw1@users.noreply.github.com>
2022-04-28 00:18:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -655,8 +655,8 @@ loader.load("/assets/cart.glb", (glb) => {
 
 loader.load("/assets/Car.glb", (glb) => {
   var model = glb.scene
-  model.position.set(-1,1,1)
-  model.scale.set(1,1,1)
+  model.position.set(1,1,1)
+  model.scale.set(2,2,2)
   model.traverse(function(e) {
     if (e.isMesh) {
       e.castShadow = true;
